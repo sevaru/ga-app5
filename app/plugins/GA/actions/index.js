45973b3459f89b01aac89491e1d67cb806bc125b
seva <sevaru@inbox.ru>
2016-06-20 16:53:19 +0300

    Little cleanup

--- app/plugins/GA/actions/index.js
+++ app/plugins/GA/actions/index.js
@@ -1,7 +1,6 @@
 export const OPTIONS_CHANGE_ACTION = 'optionsChangeAction';
 
 export function changeOptions(optionsPath, value) {
-	console.log(optionsPath);
 	return {
 		type: OPTIONS_CHANGE_ACTION,
 		optionsPath,
--- app/plugins/GA/components/GAOptions.jsx
+++ app/plugins/GA/components/GAOptions.jsx
@@ -26,33 +26,3 @@ export default ({store}) => (
         <FitnessProvider store={store} />
     </div>
 );
-
-/**
- * @deprecated
- */
-class GAOptions_old extends React.Component {
-    /**
-     * @deprecated
-     */
-    _renderFitnessOptions() {
-        const options = this.props.options.fitness;
-        return (
-            <Panel header="Fitness">
-                <form>
-                    <Panel header="BasicReference">
-                        <CheckboxControl onChange={this.props.onChange.bind(this, 'fitness.basicReferenceFitness')} value={this.props.options.fitness.basicReferenceFitness.on} field={'on'} />
-                    </Panel>
-                    <Panel header="Magnitude">
-                        <CheckboxControl onChange={this.props.onChange.bind(this, 'fitness.magnitudeFitness')} value={this.props.options.fitness.magnitudeFitness.on} field={'on'} />
-                    </Panel>
-                    <Panel header="Melody">
-                        <CheckboxControl onChange={this.props.onChange.bind(this, 'fitness.melodyFitness')} value={this.props.options.fitness.melodyFitness.on} field={'on'} />
-                    </Panel>
-                    <Panel header="Interval">
-                        <CheckboxControl onChange={this.props.onChange.bind(this, 'fitness.intervalFitness')} value={this.props.options.fitness.intervalFitness.on} field={'on'} />
-                    </Panel>
-                </form>
-            </Panel>
-        )
-    }
-}
