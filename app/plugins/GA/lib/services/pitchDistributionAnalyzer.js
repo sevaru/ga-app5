563663276021a4c63e9a7a644ecbb0d2d83e7884
seva <sevaru@inbox.ru>
2016-06-21 17:56:08 +0300

    Pitch distribution analyzer

---
+++ app/plugins/GA/lib/services/pitchDistributionAnalyzer.js
@@ -0,0 +1,39 @@
+import { chunk } from 'lodash';
+import Composition from '../utils/Composition';
+import { normalizePitchToOctave } from '../utils/normalization';
+import MusicContext from '../MusicContext';
+
+// TODO: test this module it
+
+function calculateDistributionForBar(bar, availableValues) {
+	const barSize = bar.length;
+
+	const obj = {};
+	// 1. Prepopulate with initial values
+	availableValues.forEach(v => {
+		obj[v] = 0;
+	});
+
+	// 2. Summ
+	bar.forEach(g => {
+		obj[g]++;
+	});
+
+	// 3. Normalize
+	availableValues.forEach(v => {
+		obj[v] = obj[v] / barSize;
+	});
+
+	return obj;
+}
+
+export function pitchDistributionAnalyzer(data, ignoreOctaves = false) {
+	const normalizedData = ignoreOctaves ? normalizePitchToOctave(data) : data;
+	const availableValues = MusicContext.getAvailablePitchValues(ignoreOctaves);
+	const barSize = MusicContext.getBarLength();
+	const bars = chunk(normalizedData, barSize);
+
+	return bars
+		.map(bar =>
+			calculateDistributionForBar(bar, availableValues));
+}
