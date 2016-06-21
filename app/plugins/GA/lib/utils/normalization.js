6e7a5d0431af85f3f4105d8b87c224c50c64fc4c
seva <sevaru@inbox.ru>
2016-06-21 17:55:50 +0300

    normalizePitchToOctave
    normalize all pitches within octave

--- app/plugins/GA/lib/utils/normalization.js
+++ app/plugins/GA/lib/utils/normalization.js
@@ -1,3 +1,6 @@
+import MusicContext from '../MusicContext';
+import { numberUtils } from '../utils';
+
 /**
  * Replace holds with pauses
  * @type {Array<Number>}
@@ -8,6 +11,10 @@ export const normalizeHolds =
 		.reduce((store, current) => 
 			store.concat(current === -1 ? 0 : current), []);
 
+/**
+ * Replace holds with previous notePitchs/pauses
+ * @example 7, -1, -1, 0, -1 = 7, 7, 7, 0, 0
+ */
 export const normalizeNotesDiscrete =
 	content => 
 		content
@@ -19,3 +26,19 @@ export const normalizeNotesDiscrete =
 			return store;
 		}, { data: [], note: 0 })
 		.data;
+
+
+// TODO: probably could be dynamic, for now static
+const MAX_ONE_OCTAVE_VALUE = MusicContext.getMaxOneOctaveValue();
+export const normalizePitchToOctave =
+	content => 
+		content
+		.map(v => numberUtils.normalizeValue(v, MAX_ONE_OCTAVE_VALUE));
+
+
+export const normalizer =
+	normalizers =>
+		content =>
+			normalizers
+				.reduce((data, normalizerFn) =>
+					normalizerFn(data), content);
