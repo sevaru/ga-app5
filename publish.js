40fc000217bb08d0bd7b7a0e5567cc0757077f85
Seva <sevaru@inbox.ru>
2016-09-01 00:25:26 +0300

    testing git creds

--- publish.js
+++ publish.js
@@ -3,5 +3,4 @@ const path = require('path');
  
 ghpages.publish(
 	path.join(__dirname, 'build'), 
-	err => console.log('Problem with publishing to gh-pages', err)
-);
+	err => console.log('Problem with publishing to gh-pages', err));
