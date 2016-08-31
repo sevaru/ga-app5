b4a0e3d9d0d2f95e56db8c3c6f4b1b766cc26b8f
seva <sevaru@inbox.ru>
2016-08-31 22:59:50 +0300

    Little cleanup

--- app/components/layout/LayoutSideBar.jsx
+++ app/components/layout/LayoutSideBar.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-//import nav from '../../nav.js';
 import { Link } from 'react-router';
 import { Col, Nav, NavItem, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default class LayoutSideBar extends React.Component {
     render() {
-
         const navItems = this.props.nav.map(navItem => {
             return (
                 <LinkContainer key={navItem.path} to={navItem.path}>
