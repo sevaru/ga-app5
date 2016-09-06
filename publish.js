1198ed9cc88bbe3c23dd57ce4c727b09daf60f95
Seva <sevaru@inbox.ru>
2016-09-07 00:03:12 +0300

    publish script fixes + new build

--- publish.js
+++ publish.js
@@ -3,4 +3,4 @@ const path = require('path');
  
 ghpages.publish(
 	path.join(__dirname, 'build'), 
-	err => console.log('Problem with publishing to gh-pages', err));
+	err => err && console.log('Problem with publishing to gh-pages', err));
