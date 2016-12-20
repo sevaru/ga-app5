02e9a72219832b2dcb28fc66f563cfe15b42d520
Seva <sevaru@inbox.ru>
2016-12-20 23:11:25 +0300

    little style fix

--- app/plugins/GA/components/GAOptions.jsx
+++ app/plugins/GA/components/GAOptions.jsx
@@ -1,21 +1,10 @@
-// 3d Party
 import React from 'react';
+import { Component as CrossoverProvider } from '../lib/crossovers/CrossoverProvider';
+import { Component as MutationProvider } from '../lib/mutations/MutationProvider';
+import { Component as FitnessProvider } from '../lib/fitness/FitnessProvider';
+import { render as GAOptionsComponent } from '../lib/ga/options';
 
-// Local
-import {Component as CrossoverProvider} from '../lib/crossovers/CrossoverProvider';
-import {Component as MutationProvider} from '../lib/mutations/MutationProvider';
-import {Component as FitnessProvider} from '../lib/fitness/FitnessProvider';
-import {render as GAOptionsComponent} from '../lib/ga/options';
-
-/*
-should call render methods on UIProviders
-
-import ProvidersContext from 'providers-context';
-
-ProvidersContext.forEach(() => )
- */
-
-export default ({store}) => (
+export default ({ store }) => (
     <div>
         <GAOptionsComponent store={store} />
         <CrossoverProvider store={store} />
